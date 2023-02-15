9ea163993ae40033521dd1824f4caa97e47e4813
Sg <suica@users.noreply.github.com>
2023-02-16 00:50:20 +0800

    ci: set baseUrl for github pages

---
+++ docs/docusaurus.config.github-pages.js
@@ -0,0 +1,10 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+import oldConfig from './docusaurus.config';
+
+const config = {
+    ...oldConfig,
+    baseUrl: '/write-you-a-typescript/',
+};
+
+module.exports = config;
