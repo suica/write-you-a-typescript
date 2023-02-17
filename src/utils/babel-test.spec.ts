09bab78d9712ba94629e7c8542b5ff0aef2cd590
Sg <suica@users.noreply.github.com>
2023-02-17 13:12:29 +0800

    style: prettier code

--- src/utils/babel-test.spec.ts
+++ src/utils/babel-test.spec.ts
@@ -1,8 +1,8 @@
-import { compileAsJS } from "./babel-test";
+import { compileAsJS } from './babel-test';
 
-describe('compileAsJS',()=>{
+describe('compileAsJS', () => {
     it('should work for simple code', () => {
         const code = compileAsJS('const a:number = 1; //test');
         expect(code).toBe('const a = 1; //test');
     });
-})
+});
