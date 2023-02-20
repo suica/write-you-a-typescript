47001bf90ff117cd85b50ec7bdb507711d17ccfb
Sg <suica@users.noreply.github.com>
2023-02-20 22:41:35 +0800

    refactor: replace utils

--- src/tat-stlc/checker.ts
+++ src/tat-stlc/checker.ts
@@ -13,6 +13,8 @@ import {
     TATTypeEnum,
     TATTypeParameter,
     TATUnitType,
+    isListSubtypeOf,
+    isSubtypeOf,
     isTypeEqual,
 } from './TATTypes';
 import { TypingContext } from './TypingContext';
@@ -29,43 +31,6 @@ function assertGetIdentifierName(node: Node): string {
 function todoAddDiagnostics(msg: string = ''): never {
     throw new Error(msg || 'TODO: add diagnostics');
 }
-
-const isListSubtypeOf = (s: (TATType | undefined)[], t: (TATType | undefined)[]) => {
-    const nonNilS = s.filter(isNotNil);
-    const nonNilT = t.filter(isNotNil);
-    if (nonNilT.length !== t.length || nonNilS.length !== s.length) {
-        return false;
-    }
-    if (nonNilS.length === t.length) {
-        return nonNilS.every((typ, index) => {
-            return isSubtypeOf(typ, nonNilT[index]);
-        });
-    }
-    return false;
-};
-const isSubtypeOf = (s: TATType, t: TATType): boolean => {
-    // 判断一下s是否是t的子类型
-    if (t.type === TATTypeEnum.Top) {
-        return true;
-    } else if (s.type === TATTypeEnum.Fun && t.type === TATTypeEnum.Fun) {
-        // 是函数！
-        const s1 = s.from;
-        const s2 = s.to;
-        const t1 = t.from;
-        const t2 = t.to;
-        return isListSubtypeOf(t1, s1) && isSubtypeOf(s2, t2);
-    } else if (t.type === TATTypeEnum.Obj && s.type === TATTypeEnum.Obj) {
-        return Object.keys(t.mapping).every((key) => {
-            if (s.mapping[key]) {
-                return isSubtypeOf(s.mapping[key], t.mapping[key]);
-            }
-            return false;
-        });
-    } else if (s.type === TATTypeEnum.Reference && isSubtypeOf(s.subtypeOf, t)) {
-        return true;
-    }
-    return isTypeEqual(s, t);
-};
 
 class Checker {
     typeMap: NodeTypeMap = new WeakMap();
