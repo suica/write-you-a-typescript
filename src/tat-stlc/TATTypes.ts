d5b889a391525a5da85640beddcac54b6d82e40c
Sg <suica@users.noreply.github.com>
2023-02-20 22:50:20 +0800

    chore: add comments to code

--- src/tat-stlc/TATTypes.ts
+++ src/tat-stlc/TATTypes.ts
@@ -52,18 +52,26 @@ export const isListSubtypeOf = (s: (TATType | undefined)[], t: (TATType | undefi
     }
     return false;
 };
+
+/**
+ * s 是否是 t 的子类型
+ * @param s
+ * @param t
+ * @returns
+ */
 export const isSubtypeOf = (s: TATType, t: TATType): boolean => {
-    // 判断一下s是否是t的子类型
+    // s必定是Top的子类型
     if (t.type === TATTypeEnum.Top) {
         return true;
     } else if (s.type === TATTypeEnum.Fun && t.type === TATTypeEnum.Fun) {
-        // 是函数！
+        // s和t都是函数，使用函数的子类型规则
         const s1 = s.from;
         const s2 = s.to;
         const t1 = t.from;
         const t2 = t.to;
         return isListSubtypeOf(t1, s1) && isSubtypeOf(s2, t2);
     } else if (t.type === TATTypeEnum.Obj && s.type === TATTypeEnum.Obj) {
+        // s和t都是对象，使用结构化子类型规则
         return Object.keys(t.mapping).every((key) => {
             if (s.mapping[key]) {
                 return isSubtypeOf(s.mapping[key], t.mapping[key]);
@@ -71,8 +79,10 @@ export const isSubtypeOf = (s: TATType, t: TATType): boolean => {
             return false;
         });
     } else if (s.type === TATTypeEnum.Reference && isSubtypeOf(s.subtypeOf, t)) {
+        // s是类型引用，看看s的上界是否是t的子类型
         return true;
     }
+    // 其他情形：判断s和t是否相等
     return isTypeEqual(s, t);
 };
 
